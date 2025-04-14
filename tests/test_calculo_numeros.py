db197bb65fca136655e5dba070bc18645b965fdd
josefinaPorolli <j.porolli@alumno.um.edu.ar>
2025-04-14 17:05:32 -0300

    Fase 1: Tests - números válidos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,8 +1,15 @@
 import unittest
+
+# Para detectar el directorio src
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.exceptions import (
     ingrese_numero,
     NumeroDebeSerPositivo,
 )
+
 from unittest.mock import patch
 
 class TestCalculoNumeros(unittest.TestCase):
@@ -15,21 +22,6 @@ class TestCalculoNumeros(unittest.TestCase):
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
 
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='-100'
-    )
-    def test_ingreso_negativo(self, patch_input):
-        with self.assertRaises(NumeroDebeSerPositivo):
-            ingrese_numero()
-
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='AAA'
-    )
-    def test_ingreso_letras(self, patch_input):
-        with self.assertRaises(ValueError):
-            ingrese_numero()
 
 if __name__ == '__main__':
     unittest.main() 
